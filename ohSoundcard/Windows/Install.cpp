a0b4b7883b5afa80238707c8b67e3d4252949560
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2011-09-12 18:07:16 +0100

    Updated so that you can specify which driver to install. Updated so that you can delete a specified driver.

--- ohSoundcard/Windows/Install.cpp
+++ ohSoundcard/Windows/Install.cpp
@@ -41,7 +41,7 @@ static const TUint KSPROPERTY_OHSOUNDCARD_ACTIVE = 2;
 static const TUint KSPROPERTY_OHSOUNDCARD_ENDPOINT = 3;
 static const TUint KSPROPERTY_OHSOUNDCARD_TTL = 4;
 
-TBool FindDriver()
+TBool FindDriver(const char* aHardwareId)
 {
     HDEVINFO deviceInfoSet = SetupDiGetClassDevs(&KSCATEGORY_AUDIO, 0, 0, DIGCF_DEVICEINTERFACE | DIGCF_PROFILE | DIGCF_PRESENT);
 
@@ -67,41 +67,49 @@ TBool FindDriver()
 
 	deviceInterfaceDetailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
 
-	for (TUint i = 0; SetupDiEnumDeviceInterfaces(deviceInfoSet, 0, &KSCATEGORY_AUDIO, i, &deviceInterfaceData); i++)
+    for (TUint i = 0; SetupDiEnumDeviceInterfaces(deviceInfoSet, 0, &KSCATEGORY_AUDIO, i, &deviceInterfaceData); i++)
 	{
 		// now we can get some more detailed information
 
-		if (SetupDiGetDeviceInterfaceDetail(deviceInfoSet, &deviceInterfaceData, deviceInterfaceDetailData, 1000, 0, &deviceInfoData))
-		{
-			try
-			{
-				HANDLE handle = CreateFile(deviceInterfaceDetailData->DevicePath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
+        if (SetupDiGetDeviceInterfaceDetail(deviceInfoSet, &deviceInterfaceData, deviceInterfaceDetailData, 1000, 0, &deviceInfoData))
+        {
+            char buffer[1024];
+            if (SetupDiGetDeviceRegistryProperty(deviceInfoSet, &deviceInfoData, SPDRP_HARDWAREID, NULL, (LPBYTE)buffer, 1024, NULL)) {
+                printf("%s\n", buffer);
+                if(strcmp(aHardwareId, buffer) == 0) {
+                    try
+                    {
+                        printf("Found a valid driver: %s\n", deviceInterfaceDetailData->DevicePath);
+                        
+                        HANDLE handle = CreateFile(deviceInterfaceDetailData->DevicePath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
 
-                KSPROPERTY prop;
-				
-				prop.Set = OHSOUNDCARD_GUID;
-                prop.Id = KSPROPERTY_OHSOUNDCARD_VERSION;
-                prop.Flags = KSPROPERTY_TYPE_GET;
+                        KSPROPERTY prop;
+                        
+                        prop.Set = OHSOUNDCARD_GUID;
+                        prop.Id = KSPROPERTY_OHSOUNDCARD_VERSION;
+                        prop.Flags = KSPROPERTY_TYPE_GET;
 
-                TByte buffer[4];
+                        TByte buffer[4];
 
-                DWORD bytes;
+                        DWORD bytes;
 
-                if (DeviceIoControl(handle, IOCTL_KS_PROPERTY, &prop, sizeof(KSPROPERTY), buffer, sizeof(buffer), &bytes, 0))
-                {
-					TUint version = *(TUint*)buffer;
+                        if (DeviceIoControl(handle, IOCTL_KS_PROPERTY, &prop, sizeof(KSPROPERTY), buffer, sizeof(buffer), &bytes, 0))
+                        {
+                            TUint version = *(TUint*)buffer;
 
-					if (version == 1) {
-						delete [] detail;
-						SetupDiDestroyDeviceInfoList(deviceInfoSet);
-						return (true);
-					}
+                            if (version == 1) {
+                                delete [] detail;
+                                SetupDiDestroyDeviceInfoList(deviceInfoSet);
+                                return (true);
+                            }
+                        }
+                    }
+                    catch (...)
+                    {
+                    }
                 }
-			}
-			catch (...)
-			{
-			}
-		}
+            }
+        }
 	}
 
 	delete [] detail;
@@ -111,9 +119,9 @@ TBool FindDriver()
 	return (false);
 }
 
-TBool InstallDriver()
+TBool InstallDriver(const char* aHardwareId, const char* aInfFilename)
 {
-	printf("Installing\n");
+	printf("Installing %s from %s\n", aHardwareId, aInfFilename);
 
 	HDEVINFO deviceInfoSet = SetupDiCreateDeviceInfoList(&KSCATEGORY_AUDIO, 0);
 
@@ -144,7 +152,7 @@ TBool InstallDriver()
 		return (false);
     }
 
-	char* hwid = "{D6BAC7AB-8758-43A9-917F-D702501F4DB0}\\ohSoundcard\0\0";
+	//char* hwid = "{D6BAC7AB-8758-43A9-917F-D702501F4DB0}\\Soundkard\0\0";
 
     //
     // Add the HardwareID to the Device's HardwareID property.
@@ -152,8 +160,8 @@ TBool InstallDriver()
     if (!SetupDiSetDeviceRegistryProperty(deviceInfoSet,
         &deviceInfoData,
         SPDRP_HARDWAREID,
-        (LPBYTE)hwid,
-        (lstrlen(hwid))))
+        (LPBYTE)aHardwareId,
+        (lstrlen(aHardwareId))))
     {
 		printf("3 %d\n", GetLastError());
         SetupDiDestroyDeviceInfoList(deviceInfoSet);
@@ -183,7 +191,8 @@ TBool InstallDriver()
     // Convert inf to full pathname
     //
 
-    int res = GetFullPathName("driver\\ohSoundcard.inf", 200, inf, NULL);
+    //int res = GetFullPathName("Soundkard.inf", 200, inf, NULL);
+    int res = GetFullPathName(aInfFilename, 200, inf, NULL);
 
     if ((res >= 200) || (res == 0)) {
 		printf("5 %d\n", GetLastError());
@@ -220,7 +229,7 @@ TBool InstallDriver()
 	printf(inf);
 	printf("\n");
 
-	if (!updateFunction(NULL, hwid, inf, INSTALLFLAG_FORCE, &reboot)) {
+	if (!updateFunction(NULL, aHardwareId, inf, INSTALLFLAG_FORCE, &reboot)) {
 		printf("9 %d\n", GetLastError());
         FreeLibrary(newDevDll);
 		return (false);
@@ -231,16 +240,130 @@ TBool InstallDriver()
 	return (true);
 }
 
-int __cdecl main(int /* aArgc */, char* /* aArgv[] */)
+TBool DeleteDriver(const char* aHardwareId, const char* aInfFilename)
 {
-	if (!FindDriver()) {
-		if (!InstallDriver()) {
-			return (1);
-		}
-		if (!FindDriver()) {
-			return (1);
-		}
+	printf("Deleting %s from %s\n", aHardwareId, aInfFilename);
+    
+    HDEVINFO deviceInfoSet = SetupDiGetClassDevs(&KSCATEGORY_AUDIO, 0, 0, DIGCF_DEVICEINTERFACE | DIGCF_PROFILE | DIGCF_PRESENT);
+
+	if (deviceInfoSet == 0) {
+		return (false);
 	}
+
+	// build a DevInfo Data structure
+
+	SP_DEVINFO_DATA deviceInfoData;
+
+	deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+
+	for (TUint i = 0; SetupDiEnumDeviceInfo(deviceInfoSet, i, &deviceInfoData); i++)
+	{
+        char buffer[1024];
+        if (SetupDiGetDeviceRegistryProperty(deviceInfoSet, &deviceInfoData, SPDRP_HARDWAREID, NULL, (LPBYTE)buffer, 1024, NULL)) {
+            printf("%s\n", buffer);
+            if(strcmp(aHardwareId, buffer) == 0) {
+                if (SetupDiBuildDriverInfoList(deviceInfoSet, &deviceInfoData, SPDIT_COMPATDRIVER)) {
+                    SP_DRVINFO_DATA driverInfoData;
+                    
+                    driverInfoData.cbSize = sizeof(SP_DRVINFO_DATA);
+                    
+                    SP_DRVINFO_DETAIL_DATA driverInfoDetail;
+                    
+                    driverInfoDetail.cbSize = sizeof(SP_DRVINFO_DETAIL_DATA);
+                    
+                    if (SetupDiEnumDriverInfo(deviceInfoSet, &deviceInfoData, SPDIT_COMPATDRIVER, 0, &driverInfoData)) {
+                        bool deleteFiles = true;
+                        if (!SetupDiGetDriverInfoDetail(deviceInfoSet, &deviceInfoData, &driverInfoData, &driverInfoDetail, sizeof(driverInfoDetail), NULL)) {
+                            HRESULT result = GetLastError();
+                            if (result != ERROR_INSUFFICIENT_BUFFER) {
+                                printf("1 %d\n", GetLastError());
+                                deleteFiles = false;
+                            }
+                        }
+                        
+                        if (deleteFiles) {
+                            size_t length = strlen(driverInfoDetail.InfFileName);
+                            char pnfFilename[200];
+                            strncpy(pnfFilename, driverInfoDetail.InfFileName, length - 3);
+                            strcpy(pnfFilename + length - 3, "pnf");
+                            
+                            if (DeleteFile(driverInfoDetail.InfFileName)) {
+                                printf("Deleted %s\n", driverInfoDetail.InfFileName);
+                            }
+                            else {
+                                printf("Couldn't find %s\n", driverInfoDetail.InfFileName);
+                            }
+                            if (DeleteFile(pnfFilename)) {
+                                printf("Deleted %s\n", pnfFilename);
+                            }
+                            else {
+                                printf("Couldn't find %s\n", pnfFilename);
+                            }
+                        }
+                    }
+                    else {
+                        printf("2 %d\n", GetLastError());
+                    }
+                }
+                else {
+                    printf("3 %d\n", GetLastError());
+                }
+        
+                if(SetupDiCallClassInstaller(DIF_REMOVE, deviceInfoSet, &deviceInfoData)) {
+                    printf("Deleted device with hardware id %d\n", aHardwareId);
+                }
+                else {
+                    printf("4 %d\n", GetLastError());
+                }
+            }            
+        }
+        else {
+            printf("5 %d\n", GetLastError());
+        }
+	}
+
+	SetupDiDestroyDeviceInfoList(deviceInfoSet);
+    
+    return (true);
+}
+
+int __cdecl main(int aArgc, char* aArgv[])
+{
+    if(aArgc != 4)
+    {
+        printf("Incorrect number of args\n\n");
+        printf("Usage:\n");
+        printf("Install driver and device:  InstallXX.exe -i <Hardware Id> <Inf>\n");
+        printf("Delete driver and device :  InstallXX.exe -d <Hardware Id> <Inf>\n");
+        return(1);
+    }
+    
+	
+    if (strcmp(aArgv[1], "-i") == 0) {
+        if (!FindDriver(aArgv[2])) {
+            if (!InstallDriver(aArgv[2], aArgv[3])) {
+                return (1);
+            }
+            if (!FindDriver(aArgv[2])) {
+                return (1);
+            }
+        }
+    }
+    else if (strcmp(aArgv[1], "-d") == 0) {
+        if(!DeleteDriver(aArgv[2], aArgv[3])) {
+            return (1);
+        }
+        if (FindDriver(aArgv[2])) {
+            return (1);
+        }
+    }
+    else {
+        printf("Unrecognised option %s\n\n", aArgv[1]);
+        printf("Usage:\n");
+        printf("Install driver and device:  InstallXX.exe -i <Hardware Id> <Inf>\n");
+        printf("Delete driver and device :  InstallXX.exe -d <Hardware Id> <Inf>\n");
+    }
+    
 	return (0);
 }
 
