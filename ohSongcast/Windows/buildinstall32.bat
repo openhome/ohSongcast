4c816a2b04bc625e6ed9d696572bb104d104bfcc
Steven Spencer <steven.spencer@linn.co.uk>
2011-12-13 11:28:04 +0000

    Update some more windows songcast build scripts to have correct signing flags

--- ohSongcast/Windows/buildinstall32.bat
+++ ohSongcast/Windows/buildinstall32.bat
@@ -1,5 +1,5 @@
 cl /nologo /FoBuild/Obj/Install32.obj -c /W4 /WX /EHsc /Gd -DDEFINE_LITTLE_ENDIAN -DDEFINE_TRACE -D_CRT_SECURE_NO_WARNINGS -DDllExport=__declspec(dllexport) -DDllExportClass= Install.cpp -I..\..\..\ohNet\Build\Include -I..\..\..\ohNet\Build\Include\Cpp
 link /SUBSYSTEM:windows /ENTRY:mainCRTStartup /nologo /map Iphlpapi.lib /incremental:no /out:Build\Obj\Install32.exe Build\Obj\Install32.obj setupapi.lib
 copy Build\Obj\Install32.exe Build\Driver32\Install32.exe
-signtool sign /v /a /t http://timestamp.verisign.com/scripts/timestamp.dll Build\Driver32\Install32.exe
+signtool sign /v /a /sm /t http://timestamp.verisign.com/scripts/timestamp.dll Build\Driver32\Install32.exe
 copy Build\Obj\Install32.exe Build\Install32.exe
