4f68fb1bfacf00ebcc07a6082aac0788f5eb84c8
Steven Spencer <steven.spencer@linn.co.uk>
2011-12-22 10:58:47 +0000

    Fixed macro bug with mac driver code

--- ohSongcast/Mac/Driver/KextInfo.c
+++ ohSongcast/Mac/Driver/KextInfo.c
@@ -2,10 +2,14 @@
 #include <mach/mach_types.h>
 #include "Branding.h"
 
+// additional layer of macro to ensure that BRANDING_KEXTINFO_KMODNAME gets expanded - this is because the
+// KMOD_EXPLICIT_DECL macro uses the stringify operator (#) for the first argument
+#define KMOD_DECL_LAYER(KMODNAME, KMODVER, KMODSTART, KMODSTOP) KMOD_EXPLICIT_DECL(KMODNAME, KMODVER, KMODSTART, KMODSTOP)
+
 extern kern_return_t _start(kmod_info_t *ki, void *data);
 extern kern_return_t _stop(kmod_info_t *ki, void *data);
  
-__attribute__((visibility("default"))) KMOD_EXPLICIT_DECL(BRANDING_KEXTINFO_KMODNAME, BRANDING_KEXTINFO_KMODVERSION, _start, _stop)
+__attribute__((visibility("default"))) KMOD_DECL_LAYER(BRANDING_KEXTINFO_KMODNAME, BRANDING_KEXTINFO_KMODVERSION, _start, _stop)
 
 __private_extern__ kmod_start_func_t *_realmain = 0;
 __private_extern__ kmod_stop_func_t *_antimain = 0;
